fb49658128b28ba27bc1f079a08f8ecf754ac25e
Jason Spence <solar_burst@hotmail.com>
2026-01-21 17:37:55 -0500

    Updating blocks and starting dark top down

--- Scenes/Players/TopDownPlayer.cs
+++ Scenes/Players/TopDownPlayer.cs
@@ -7,7 +7,8 @@ public partial class TopDownPlayer : CharacterBody2D
 	[Export]
 	float speed = 100.0f;
 	[Export]
-	float jumpStrength = 150.0f;
+	float push_strength = 100.0f;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -18,6 +19,8 @@ public partial class TopDownPlayer : CharacterBody2D
 	{
 
 		MovePlayer();
+
+		PushBlocks();
 
 		MoveAndSlide();
 
@@ -33,21 +36,25 @@ public partial class TopDownPlayer : CharacterBody2D
 		{
 			// Play move left animation
 			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("move_left");
+			GetNode<Area2D>("InteractArea").Position = new Vector2(-7, 1);
 		} 
 		else if (movement.X > 0)
 		{
 			// Play move right animation
 			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("move_right");
+			GetNode<Area2D>("InteractArea").Position = new Vector2(7, 1);
 		}
 		else if (movement.Y > 0)
 		{
 			// Play move down animation
 			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("move_down");
+			GetNode<Area2D>("InteractArea").Position = new Vector2(0, 7);
 		}
 		else if (movement.Y < 0)
 		{
 			// Play move up animation
 			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("move_up");
+			GetNode<Area2D>("InteractArea").Position = new Vector2(0, -7);
 		}
 		else
 		{
@@ -55,5 +62,26 @@ public partial class TopDownPlayer : CharacterBody2D
 		}
 		
 		
+	}
+
+	private void PushBlocks()
+	{
+		// Get the last collision
+		// Check if it is the block
+		// if it is the block, push it
+		KinematicCollision2D collision = GetLastSlideCollision();
+		
+		if(collision != null && IsInstanceValid(collision))
+		{
+			var colliderNode = collision.GetCollider() as Node;
+			
+			
+			if(colliderNode.IsInGroup("Pushable"))
+			{
+				RigidBody2D colNode = colliderNode as RigidBody2D;
+				Vector2 collisionNormal = collision.GetNormal();
+				colNode.ApplyCentralForce(-collisionNormal * push_strength);
+			}
+		}
 	}
 }
