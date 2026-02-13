8a52c07df7bb45560daa7f443e4ee4aa29556e69
Jason Spence <solar_burst@hotmail.com>
2026-02-12 19:55:39 -0500

    Flashlight working

--- Scenes/Players/TopDownPlayer.cs
+++ Scenes/Players/TopDownPlayer.cs
@@ -37,24 +37,28 @@ public partial class TopDownPlayer : CharacterBody2D
 			// Play move left animation
 			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("move_left");
 			GetNode<Area2D>("InteractArea").Position = new Vector2(-7, 1);
+			GetNode<LightOccluder2D>("LightOccluder2D").Occluder.Polygon = TurnLightOccluder("left");
 		} 
 		else if (movement.X > 0)
 		{
 			// Play move right animation
 			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("move_right");
 			GetNode<Area2D>("InteractArea").Position = new Vector2(7, 1);
+			GetNode<LightOccluder2D>("LightOccluder2D").Occluder.Polygon = TurnLightOccluder("right");
 		}
 		else if (movement.Y > 0)
 		{
 			// Play move down animation
 			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("move_down");
 			GetNode<Area2D>("InteractArea").Position = new Vector2(0, 7);
+			GetNode<LightOccluder2D>("LightOccluder2D").Occluder.Polygon = TurnLightOccluder("down");
 		}
 		else if (movement.Y < 0)
 		{
 			// Play move up animation
 			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play("move_up");
 			GetNode<Area2D>("InteractArea").Position = new Vector2(0, -7);
+			GetNode<LightOccluder2D>("LightOccluder2D").Occluder.Polygon = TurnLightOccluder("up");
 		}
 		else
 		{
@@ -83,5 +87,78 @@ public partial class TopDownPlayer : CharacterBody2D
 				colNode.ApplyCentralForce(-collisionNormal * push_strength);
 			}
 		}
+	}
+
+	private Vector2[] TurnLightOccluder(string direction)
+	{
+		Vector2[] directionArray;
+
+		if(direction == "left")
+		{
+			directionArray = 
+				[new Vector2(2.0f, 0.0f), 
+				new Vector2(2.0f, -2.0f), 
+				new Vector2(-2.0f, -2.0f), 
+				new Vector2(-23.0f, -14.0f), 
+				new Vector2(4.0f, -2.0f), 
+				new Vector2(4.0f, 2.0f), 
+				new Vector2(-23.0f, 14.0f), 
+				new Vector2(-2.0f, 2.0f), 
+				new Vector2(2.0f, 2.0f)];
+		}
+		else if (direction =="right")
+		{
+			directionArray = 
+				[new Vector2(-2.0f, 0.0f), 
+				new Vector2(-2.0f, 2.0f), 
+				new Vector2(2.0f, 2.0f), 
+				new Vector2(23.0f, 14.0f), 
+				new Vector2(-4.0f, 2.0f), 
+				new Vector2(-4.0f, -2.0f), 
+				new Vector2(23.0f, -14.0f), 
+				new Vector2(2.0f, -2.0f), 
+				new Vector2(-2.0f, -2.0f)];
+		}
+		else if (direction == "down")
+		{
+			directionArray = 
+				[new Vector2(0.0f,-2.0f), 
+				new Vector2(-2.0f, -2.0f), 
+				new Vector2(-2.0f, 2.0f), 
+				new Vector2(-14.0f, 23.0f), 
+				new Vector2(-2.0f, -4.0f), 
+				new Vector2(2.0f, -4.0f), 
+				new Vector2(14.0f, 23.0f), 
+				new Vector2(2.0f, 2.0f), 
+				new Vector2(2.0f, -2.0f)];
+		}
+		else if (direction == "up")
+		{
+			directionArray = 
+				[new Vector2(0.0f, 2.0f), 
+				new Vector2(2.0f, 2.0f), 
+				new Vector2(2.0f, -2.0f), 
+				new Vector2(14.0f, -23.0f), 
+				new Vector2(2.0f, 4.0f), 
+				new Vector2(-2.0f, 4.0f), 
+				new Vector2(-14.0f, -23.0f), 
+				new Vector2(-2.0f, -2.0f), 
+				new Vector2(-2.0f, 2.0f)];
+		}
+		else
+		{
+			directionArray = 
+				[new Vector2(0.0f,-2.0f), 
+				new Vector2(-2.0f, -2.0f), 
+				new Vector2(-2.0f, 2.0f), 
+				new Vector2(-14.0f, 23.0f), 
+				new Vector2(-2.0f, -4.0f), 
+				new Vector2(2.0f, -4.0f), 
+				new Vector2(14.0f, 23.0f), 
+				new Vector2(2.0f, 2.0f), 
+				new Vector2(2.0f, -2.0f)];
+		}
+
+		return directionArray;
 	}
 }
